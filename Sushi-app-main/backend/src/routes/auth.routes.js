c3651c21ea9f4e9e93272757476710f461b40555
yumiNina97 <yuvinca.nina@ucb.edu.bo>
2025-06-30 12:44:36 -0400

    correciones

--- Sushi-app-main/backend/src/routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const UserController = require('../controller/UserController');
-
-const router = express.Router();
-
-router.post('/register', UserController.register);
-router.post('/login', UserController.login);
-
-module.exports = router;
--- Sushi-app-main/backend/src/routes/order.routes.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const auth = require('../middleware/authMiddleware');
-const OrderController = require('../controller/OrderController');
-
-router.post('/', auth, OrderController.create);
-
-module.exports = router;
