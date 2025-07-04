85efea16d2560288bdd2144dab788a897bd087eb
yumiNina97 <yuvinca.nina@ucb.edu.bo>
2025-07-04 15:54:30 -0400

    conexion con el backend verificando

--- backend/server.js
+++ backend/server.js
@@ -26,52 +26,6 @@ app.use('/api/blog', blogRoutes);
 app.use('/api/reservas', reservaRoutes);
 app.use('/api', require('./routes/auth'))
 
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
-const jwt = require('jsonwebtoken');
-
-const router = express.Router();
-const JWT_SECRET = process.env.JWT_SECRET || 'clave-secreta-temporal';
-
-router.post('/login', async (req, res) => {
-  const { email, contrasena } = req.body;
-
-  try {
-    
-    const filePath = path.join(__dirname, '../../frontend/data/users.json');
-    const rawData = fs.readFileSync(filePath, 'utf8');
-    const data = JSON.parse(rawData);
-
-    
-    const usuario = data.users.find(
-      (u) => u.email === email && u.contrasena_hash === contrasena
-    );
-
-    if (!usuario) {
-      return res.status(401).json({ error: 'Usuario o contraseña incorrecta' });
-    }
-
-    
-    const token = jwt.sign(
-      {
-        id: usuario.id,
-        nombre: usuario.nombre,
-        email: usuario.email,
-        rol: usuario.rol,
-      },
-      JWT_SECRET,
-      { expiresIn: '1h' }
-    );
-
-    return res.json({ token });
-  } catch (err) {
-    console.error('Error al leer el archivo JSON:', err);
-    return res.status(500).json({ error: 'Error en el servidor' });
-  }
-});
-
-module.exports = router;
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
